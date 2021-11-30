59ab865d3935090540bbba32ca1f88f3dcf17e6d
BaselAskar <76973409+BaselAskar@users.noreply.github.com>
2021-11-30 20:34:15 +0100

    Initial project

---
+++ src/main/java/com/baselcode/wrokshoplibraryspring/WrokshopLibrarySpringApplication.java
@@ -0,0 +1,13 @@
+package com.baselcode.wrokshoplibraryspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrokshopLibrarySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrokshopLibrarySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baselcode/wrokshoplibraryspring/WrokshopLibrarySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baselcode.wrokshoplibraryspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrokshopLibrarySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
