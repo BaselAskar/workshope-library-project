f24d330ebf22a36a85c0a9de19e06bddae040101
BaselAskar <76973409+BaselAskar@users.noreply.github.com>
2021-12-01 13:17:57 +0100

    Finishing part 5

--- src/main/java/com/baselcode/wrokshoplibraryspring/models/AppUser.java
+++ src/main/java/com/baselcode/wrokshoplibraryspring/models/AppUser.java
@@ -2,6 +2,8 @@ package com.baselcode.wrokshoplibraryspring.models;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class AppUser {
@@ -23,6 +25,14 @@ public class AppUser {
     )
     @JoinColumn(name = "fk_details_id")
     private Details userDetails;
+
+    @OneToMany(
+            cascade = {CascadeType.REFRESH,CascadeType.DETACH,CascadeType.PERSIST,CascadeType.MERGE},
+            fetch = FetchType.LAZY,
+            orphanRemoval = true,
+            mappedBy = "borrower"
+    )
+    private List<BookLoan> loans;
 
     public AppUser(int appUserId, String username, String password, LocalDate regDate, Details userDetails) {
         this.appUserId = appUserId;
@@ -73,5 +83,23 @@ public class AppUser {
 
     public void setUserDetails(Details userDetails) {
         this.userDetails = userDetails;
+    }
+
+    public List<BookLoan> getLoans() {
+        if(loans == null) return new ArrayList<>();
+        return loans;
+    }
+
+    public void setLoans(List<BookLoan> loans) {
+        if(loans == null) loans = new ArrayList<>();
+
+        if(loans.isEmpty()){
+            if(this.loans != null){
+                this.loans.forEach(l -> l.setBorrower(null));
+            }else {
+                loans.forEach(l -> l.setBorrower(this));
+            }
+        }
+        this.loans = loans;
     }
 }
